e266d81b7fc620b50fdddd24807e3f183a0fe19b
Drew Watkins <drew@watkins.id>
2024-07-15 21:28:51 -0400

    Update contact icons

--- app/rsvp/ContactInfo.tsx
+++ app/rsvp/ContactInfo.tsx
@@ -3,6 +3,8 @@
 import { updateInvite } from "@/app/actions";
 import { useState, useTransition } from "react";
 import { BoolInput, ControlButtons } from "./controls";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function ContactInfo(props) {
   const {
@@ -51,37 +53,21 @@ export default function ContactInfo(props) {
           </p>
         </div>
         <label className="input input-bordered flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4 opacity-70"
-          >
-            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-          </svg>
+          <FontAwesomeIcon className="mr-2" icon={faEnvelope} />
           <input
             type="email"
             className="grow"
-            placeholder="Email"
+            placeholder="you@gmail.com"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label className="input input-bordered flex items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4 opacity-70"
-          >
-            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-          </svg>
+          <FontAwesomeIcon className="mr-2" icon={faPhone} />
           <input
             type="phone"
             className="grow"
-            placeholder="Email"
+            placeholder="(123) 234-5678"
             value={phone}
             onChange={(e) => setPhone(e.target.value)}
           />
