b894757891c2f72f0191bfa9b1f888b1bf4f1208
Drew Watkins <drew@watkins.id>
2024-09-07 11:15:24 -0400

    Fix tot allow for two-character names in login

--- lib/zod.ts
+++ lib/zod.ts
@@ -2,11 +2,11 @@ import { object, string } from "zod";
 
 export const signInSchema = object({
   surname: string({ required_error: "Surname is required" }).min(
-    3,
+    2,
     "Surname is required"
   ),
   inviteId: string({ required_error: "Invite code is required" }).min(
-    3,
+    2,
     "House number is required"
   ),
   guestName: string().optional(),
